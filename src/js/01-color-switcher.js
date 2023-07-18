861250b5f53341eb7652dd2e7fdce3d12927f110
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-18 22:22:39 +0300

    start3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector(".data-start");
+const stopBtn = document.querySelector(".data-stop");
+const changeColorBody = document.querySelector("body");
+let timerId = null;
+
+stopBtn.disabled = true;
+
+const getRandomHexColor = () => {
+    let colorChange = Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
+    changeColorBody.style.backgroundColor = `#${colorChange}`;
+  }
+
+  function onClick () {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  };
+
+ startBtn.addEventListener("click", onClick);
+
+ function onRemoveClick() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+ stopBtn.addEventListener("click", onRemoveClick);
