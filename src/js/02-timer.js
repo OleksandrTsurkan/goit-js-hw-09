2eb76d072a17a5a778ef946610fc9a591914d9d4
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-22 13:51:37 +0300

    start9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,110 +1,95 @@
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const inputRef = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector("[data-start]");
-const daysRef = document.querySelector("[data-days]");
-const hoursRef = document.querySelector("[data-hours]");
-const minutesRef = document.querySelector("[data-minutes]");
-const secondsRef = document.querySelector("[data-seconds]");
+const inputEl = document.querySelector('#datetime-picker');
+const startBtnEl = document.querySelector('[data-start]');
+const daysTimer = document.querySelector('[data-days]');
+const hourTimer = document.querySelector('[data-hours]');
+const minsTimer = document.querySelector('[data-minutes]');
+const secTimer = document.querySelector('[data-seconds]');
+const timersEl = document.querySelector('.timer');
 
-let intervalId = null;
 
-// Функція для перевірки часу
-function checkTime(date) {
-  const currentDate = new Date();
-  return date > currentDate;
-}
+let days = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let timeInTimer = null;
 
-// Функція для оновлення таймера
-function updateTimer(date) {
-  const time = date - new Date();
-  if (time < 0) {
-    clearInterval(intervalId);
-    return;
-  }
+let inputedData = null;
+let timeToTimer = 0;
+const date = new Date();
+startBtnEl.disabled = true;
 
-  const { days, hours, minutes, seconds } = convertMs(time);
-  daysRef.textContent = addLeadingZero(days);
-  hoursRef.textContent = addLeadingZero(hours);
-  minutesRef.textContent = addLeadingZero(minutes);
-  secondsRef.textContent = addLeadingZero(seconds);
-}
-
-// Функція для перевірки та оновлення кнопки Start
-function updateStartButton() {
-  const selectedDate = new Date(inputRef.value);
-  if (checkTime(selectedDate)) {
-    startBtn.disabled = false;
-  } else {
-    startBtn.disabled = true;
-  }
-}
-
-// Функція для запуску таймера
-function startTimer() {
-  const selectedDate = new Date(inputRef.value);
-  if (!checkTime(selectedDate)) {
-    alert("Please choose a date in the future");
-    return;
-  }
-
-  intervalId = setInterval(() => {
-    updateTimer(selectedDate);
-  }, 1000);
-
-  startBtn.disabled = true;
-  inputRef.disabled = true;
-}
-
-// Функція для зупинки таймера
-function stopTimer() {
-  clearInterval(intervalId);
-  intervalId = null;
-
-  inputRef.disabled = false;
-  startBtn.disabled = false;
-
-  daysRef.textContent = "00";
-  hoursRef.textContent = "00";
-  minutesRef.textContent = "00";
-  secondsRef.textContent = "00";
-}
-
-// Функція для перетворення мілісекунд в дні, години, хвилини і секунди
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-// Функція для додавання 0 перед числами менше 10
-function addLeadingZero(value) {
-  return String(value).padStart(2, "0");
-}
-
-// Ініціалізація flatpickr
-flatpickr("#datetime-picker", {
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    updateStartButton(selectedDate);
+    console.log(selectedDates[0]);
+    onClose();
   },
-});
+};
+const fp = flatpickr(inputEl, options);
 
-// Додавання слухачів подій
-startBtn.addEventListener("click", startTimer);
-document.querySelector("[data-stop]").addEventListener("click", stopTimer);
+function onClose() {
+  inputedData = fp.selectedDates[0];
+  if (inputedData <= date) {
+    window.alert('Please choose a date in the future');
+  } else {
+    timeToTimer = inputedData - date;
+    startBtnEl.disabled = false;
+    timerTime();
+  }
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function timerTime() {
+  timeInTimer = convertMs(timeToTimer);
+  days = timeInTimer.days;
+  hours = timeInTimer.hours;
+  minutes = timeInTimer.minutes;
+  seconds = timeInTimer.seconds;
+
+  daysTimer.textContent = addLeadingZero(days);
+  hourTimer.textContent = addLeadingZero(hours);
+  minsTimer.textContent = addLeadingZero(minutes);
+  secTimer.textContent = addLeadingZero(seconds);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function startTimer() {
+  let myTimer = setInterval(() => {
+    startBtnEl.disabled = 'true';
+    timeToTimer -= 1000;
+    if (timeToTimer <= 0) {
+      clearInterval(myTimer);
+    } else {
+      timerTime();
+    }
+  }, 1000);
+}
+
+startBtnEl.addEventListener('click', startTimer);
